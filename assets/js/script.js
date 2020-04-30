061d8893ac7e0b2c10f6d768187b523e10215a9d
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-04-30 17:10:20 +0200

    ex6 javascript

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function calcul(){
+  var Shoe= document.getElementById('shoeSize').value;
+  var Birth = document.getElementById('yearOfBirth').value;
+  var result = (Shoe*2 + 5* 50 - yearOfBirth + 1770);
+  alert(result);}
